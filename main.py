5131a00415f2f9a1ef27e8f7729c8a57f66bbe58
Khanh Nguyen <khanhn2@illinois.edu>
2022-12-20 11:07:30 -0600

    Update the task to use multiprocess to download faster and skip downloaded albums tracked by completed_albums.json

--- main.py
+++ main.py
@@ -2,66 +2,151 @@ import os
 import requests
 from tqdm import tqdm
 import pylrc
-import shutil
+import json
 
+from PIL import Image
+from multiprocessing import Pool, Manager
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import APIC, SYLT, Encoding, ID3
 from mutagen.flac import Picture, FLAC
 from pydub import AudioSegment
-from PIL import Image
-
-
 
 
 def make_valid(filename):
-    f = filename.replace(":", "_")
-    f = f.replace("/", "_")
-    f = f.replace("<", "_")
-    f = f.replace(">", "_")
-    f = f.replace("\"", "_")
-    f = f.replace("\\", "_")
-    f = f.replace("|", "_")
-    f = f.replace("?", "_")
-    f = f.replace("*", "_")
+    # Make a filename valid in different OSs
+    f = filename.replace(':', '_')
+    f = f.replace('/', '_')
+    f = f.replace('<', '_')
+    f = f.replace('>', '_')
+    f = f.replace('\'', '_')
+    f = f.replace('\\', '_')
+    f = f.replace('|', '_')
+    f = f.replace('?', '_')
+    f = f.replace('*', '_')
     return f
+
 
 def lyric_file_to_text(filename):
     lrc_file = open(filename, 'r', encoding='utf-8')
     lrc_string = ''.join(lrc_file.readlines())
     lrc_file.close()
-
     subs = pylrc.parse(lrc_string)
-    
     ret = []
     for sub in subs:
         time = int(sub.time * 1000)
         text = sub.text
         ret.append((text, time))
-
     return ret
 
+def update_downloaded_albums(queue, directory):
+    while 1:
+        album_name = queue.get()
+        try:
+            with open(directory + 'completed_albums.json', 'r', encoding='utf8') as f:
+                completed_albums = json.load(f)
+        except:
+            completed_albums = []
+        completed_albums.append(album_name)
+        with open(directory + 'completed_albums.json', 'w+', encoding='utf8') as f:
+            json.dump(completed_albums, f)
 
-directory = './MonsterSiren/'
-try:
-    shutil.rmtree(directory)
-except:
-    pass
-try:
-    os.mkdir(directory)
-except:
-    pass
 
-session = requests.Session()
-headers = {'Accept': 'application/json'}
+def fill_metadata(filename, filetype, album, title, albumartist, artist, tracknumber, albumcover, songlyricpath):
+    if filetype == '.mp3':
+        file =  EasyID3(filename)
+    else:
+        file = FLAC(filename)
 
-# Get all albums
-albums = session.get('https://monster-siren.hypergryph.com/api/albums', headers=headers).json()['data']
-for album in albums:
-    album_cid = album['cid']
-    album_name = album['name']
-    album_coverUrl = album['coverUrl']
-    album_artistes = album['artistes']
+    file['album'] = album
+    file['title'] = title
+    file['albumartist'] = ''.join(albumartist)
+    file['artist'] = ''.join(artist)
+    file['tracknumber'] = str(tracknumber + 1)
+    file.save()
+
+    if filetype == '.mp3':
+        file = ID3(filename)
+        file.add(APIC(mime='image/png',type=3,desc='Cover',data=open(albumcover,'rb').read()))
+        # Read and add lyrics
+        if (songlyricpath != None):
+            sylt = lyric_file_to_text(songlyricpath)
+            file.setall('SYLT', [SYLT(encoding=Encoding.UTF8, lang='eng', format=2, type=1, text=sylt)])
+        file.save()
+    else:
+        image = Picture()
+        image.type = 3
+        image.desc = 'Cover'
+        image.mime = 'image/png'
+        with open(albumcover,'rb') as f:
+            image.data = f.read()
+        with Image.open(albumcover) as imagePil:
+            image.width, image.height = imagePil.size
+            image.depth = 24
+        file.add_picture(image)
+        # Read and add lyrics
+        if (songlyricpath != None):
+            musiclrc = open(songlyricpath, 'r', encoding='utf-8').read()
+            file['lyrics'] = musiclrc
+        file.save()
+
+    return 
+
+
+def download_song(session, directory, name, url):
+    source = session.get(url, stream=True)
+    filename = directory + '/' + make_valid(name)
+    filetype = ''
+
+    if source.headers['content-type'] == 'audio/mpeg':
+        filename += '.mp3'
+        filetype = '.mp3'
+    else:
+        filename += '.wav'
+
+    # Download song
+    total = int(source.headers.get('content-length', 0))
+    with open(filename, 'w+b') as f, tqdm(
+        desc=name,
+        total=total,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in source.iter_content(chunk_size = 1024):
+            size = f.write(data)
+            bar.update(size)
+
+    # If file is .wav then export to .flac
+    if source.headers['content-type'] != 'audio/mpeg':
+        AudioSegment.from_wav(filename).export(directory + '/' + make_valid(name) + '.flac', format='flac')
+        os.remove(filename)
+        filename = directory + '/' + make_valid(name) + '.flac'
+        filetype = '.flac'
+
+    return filename, filetype
+
+
+def download_album( args):
+    directory = args['directory']
+    session = args['session']
+    queue = args['queue']
+
+    album_cid = args['cid']
+    album_name = args['name']
+    album_coverUrl = args['coverUrl']
+    album_artistes = args['artistes']
     album_url = 'https://monster-siren.hypergryph.com/api/album/' + album_cid + '/detail'
+
+    try:
+        with open(directory + 'completed_albums.json', 'r', encoding='utf8') as f:
+            completed_albums = json.load(f)
+    except:
+        completed_albums = []
+
+    if album_name in completed_albums:
+        # If album is completed then skip
+        print(f'Skipping downloaded album {album_name}')
+        return
 
     try:
         os.mkdir(directory + album_name)
@@ -77,97 +162,72 @@ for album in albums:
     cover.save(directory + album_name + '/cover.png')
     os.remove(directory + album_name + '/cover.jpg')
 
-    songs = session.get(album_url, headers=headers).json()['data']['songs']
+
+    songs = session.get(album_url, headers={'Accept': 'application/json'}).json()['data']['songs']
     for song_track_number, song in enumerate(songs):
+        # Get song details
         song_cid = song['cid']
         song_name = song['name']
         song_artists = song['artistes']
         song_url = 'https://monster-siren.hypergryph.com/api/song/' + song_cid
-
-        # Get song detail
-        song_detail = session.get(song_url, headers=headers).json()['data']
+        song_detail = session.get(song_url, headers={'Accept': 'application/json'}).json()['data']
         song_lyricUrl = song_detail['lyricUrl']
         song_sourceUrl = song_detail['sourceUrl']
 
         # Download lyric
         if (song_lyricUrl != None):
-            with open(directory + album_name + '/' + make_valid(song_name) + '.lrc', 'w+b') as f:
+            songlyricpath = directory + album_name + '/' + make_valid(song_name) + '.lrc'
+            with open(songlyricpath, 'w+b') as f:
                 f.write(session.get(song_lyricUrl).content)
-
-
-        source = session.get(song_sourceUrl, stream=True)
-        filename = directory + album_name + '/' + make_valid(song_name)
-        if source.headers['content-type'] == 'audio/mpeg':
-            filename += '.mp3'
         else:
-            filename += '.wav'
+            songlyricpath = None
 
-        # Download song
-        total = int(source.headers.get('content-length', 0))
-        with open(filename, "w+b") as f, tqdm(
-            desc=song_name,
-            total=total,
-            unit='iB',
-            unit_scale=True,
-            unit_divisor=1024,
-        ) as bar:
-            for data in source.iter_content(chunk_size = 1024):
-                size = f.write(data)
-                bar.update(size)
-
-        if source.headers['content-type'] != 'audio/mpeg':
-            # If file is .wav then export to .flac
-            AudioSegment.from_wav(filename).export(directory + album_name + '/' + make_valid(song_name) + '.flac', format='flac')
-            os.remove(filename)
-            filename = directory + album_name + '/' + make_valid(song_name) + '.flac'
-
-
-        # Write metadata
-        if source.headers['content-type'] == 'audio/mpeg':
-            # Metadata for mp3 using ID3
-            file =  EasyID3(filename)
-            file['album'] = album_name
-            file['title'] = song_name
-            file['albumartist'] = ''.join(album_artistes)
-            file['artist'] = ''.join(song_artists)
-            file['tracknumber'] = str(song_track_number + 1)
-            file.save()
-            file = ID3(filename)
-            file.add(APIC(mime='image/png',type=3,desc=u'Cover',data=open(directory + album_name + '/cover.png','rb').read()))
-            # Read and add lyrics
-            if (song_lyricUrl != None):
-                sylt = lyric_file_to_text(directory + album_name + '/' + make_valid(song_name) + '.lrc')
-                file.setall("SYLT", [SYLT(encoding=Encoding.UTF8, lang='eng', format=2, type=1, text=sylt)])
-            file.save()
-
-        else:
-            # Metadata for flac using Vorbis
-            file = FLAC(filename)
-            file['album'] = album_name
-            file['title'] = song_name
-            file['albumartist'] = ''.join(album_artistes)
-            file['artist'] = ''.join(song_artists)
-            file['tracknumber'] = str(song_track_number + 1)
-
-            image = Picture()
-            image.type = 3
-            image.desc = u'Cover'
-            image.mime = u"image/png"
-            with open(directory + album_name + '/cover.png','rb') as f:
-                image.data = f.read()
-            with Image.open(directory + album_name + '/cover.png') as imagePil:
-                image.width, image.height = imagePil.size
-                image.depth = 24
-            
-            file.add_picture(image)
-            
-            if (song_lyricUrl != None):
-                musiclrc = open(directory + album_name + '/' + make_valid(song_name) + '.lrc', "r", encoding='utf-8').read()
-                file['lyrics'] = musiclrc
-
-            file.save()
+        # Download song and fill out metadata
+        filename, filetype = download_song(session=session, directory=directory + album_name, name=song_name, url=song_sourceUrl)
+        fill_metadata(filename=filename,
+                        filetype=filetype,
+                        album=album_name,
+                        title=song_name,
+                        albumartist=album_artistes,
+                        artist=song_artists,
+                        tracknumber=song_track_number,
+                        albumcover=directory + album_name + '/cover.png',
+                        songlyricpath=songlyricpath)
     
+    # Mark album as finished
+    queue.put(album_name)
+
+    return
+
+
+def main():
+    directory = './MonsterSiren/'
+    session = requests.Session()
+    manager = Manager()
+    queue = manager.Queue()
+
+    try:
+        os.mkdir(directory)
+    except:
+        pass
+
+    
+    # Get all albums
+    albums = session.get('https://monster-siren.hypergryph.com/api/albums', headers={'Accept': 'application/json'}).json()['data']
+    for album in albums:
+        album['directory'] = directory
+        album['session'] = session
+        album['queue'] = queue
+
+
+    with Pool(maxtasksperchild=1) as pool:
+        pool.apply_async(update_downloaded_albums, (queue, directory))
+        pool.map(download_album, albums)
+        queue.put('kill')
+    
+    return
 
 
 
-
+if __name__ == '__main__':
+    main()
